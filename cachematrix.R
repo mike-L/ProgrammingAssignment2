7032e62c676e7fb372f4439d70b78ce36b321d81
Michael Leung <mick.leung@gmail.com>
2015-07-15 14:10:21 +1000

    Added comments to code

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script contains two functions, 'MakeCacheMatrix' and 'cacheSolve' that
+## invert a matrix and caches it for later reuse.
+## For this script to work, the input matrix needs to be invertible
+
+##sample output:
+## > m <- matrix(1:4, 2, 2) # define a matrix
+## > m
+## [,1] [,2]
+## [1,]    1    3
+## [2,]    2    4
+## > x <- makeCacheMatrix(m) #create a list of functions around matrix m
+## > cacheSolve(x) ##first time run, matrix will get inverted
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+# > cacheSolve(x) ##second time run - inverted matrix will be retrived from cache
+# getting cached data
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+
 
 ## Write a short comment describing this function
+## makeCacheMatrix is a function that takes a matrix and returns a list of functions
+## including:
+## - get() - which returns the input matrix
+## - set() - which caches an input matrix
+## - getInverse() - which looks to see if an inverse of the matrix has been cached
 
 makeCacheMatrix <- function(x = matrix()) {
   ci <- NULL
@@ -12,7 +36,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function which takes an instance of the makeCacheMatrix function
+## which has been run on a given matrix. It checks if the inverse has already 
+## been calculated and returns the cached inverse if available. If a cached inverse
+## is not available, it will calculate one and then cache this for future use.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
